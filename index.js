b5d27730ceac4319ac62c8af035ac1d17d6cc7e0
claire <clairebontempo@gmail.com>
2021-02-11 10:57:25 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+
+  static capitalize (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize (string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string){
+    let lowerCaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let newString = string.split(" ").map(word => {
+      let filter = lowerCaseWords.find(element => element === word)
+      if (!!filter && filter !== string[0]){
+        word = word
+      } else {
+        word = word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      return word
+    })
+    return newString.join(" ")
+  }
 }
